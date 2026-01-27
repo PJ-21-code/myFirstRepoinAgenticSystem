fc2deea05cfb7a82e07990cc39362d04c3e60648
PJ-21-code <pr.pranjal21@gmail.com>
2026-01-27 21:49:29 +0530

    profile_builder file added in the folder

---
+++ python_assignment1/profile_builder.py
@@ -0,0 +1,5 @@
+name= input("Enter your name: ")
+age= int(input("Enter your age: "))
+status= bool(input("Are you an active user? write(True/False): "))
+
+print(f"User {name} is {age} years old. Active user: {status}")

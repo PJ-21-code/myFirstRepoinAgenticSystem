284072f5c7bf6c6a0fdc42e177e0f25e2972ddc2
PJ-21-code <pr.pranjal21@gmail.com>
2026-02-10 19:16:51 +0530

    employee role checker is created

---
+++ python_data_assignment/role_checker.py
@@ -0,0 +1,11 @@
+employee= ("00456","Anubhav","SALES")
+print("Employee Details--->")
+for i in employee:
+    print(i)
+
+role= {"admin", "editor", "viewer","manager"}
+
+if "admin" in role:
+    print("Admin Access: Yes")
+else:
+    print("Admin Access: No")    

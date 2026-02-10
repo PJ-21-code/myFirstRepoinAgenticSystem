d86c676735be94c576c2a5e85474a814ff7ef42a
PJ-21-code <pr.pranjal21@gmail.com>
2026-02-10 18:58:57 +0530

    student evaluation using slicing

--- python_data_assignment/student_marks.py
+++ python_data_assignment/student_marks.py
@@ -2,13 +2,6 @@ marks= [67,43,98,65,45,78,69,54,56]
 
 print("Marks of all students")
 print(marks)
-f_3=[]
-l_3=[]
-for m in range(3):
-    f_3.append(marks[m])
-
-for m in range(len(marks)-3,len(marks)):
-    l_3.append(marks[m])  
 
 print("First three marks: ",marks[:3])
 print("last three marks: ",marks[len(marks)-3:])
